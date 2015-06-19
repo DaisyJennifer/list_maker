551ac45eaad5368bc2c4c5690edbfdcf1a875969
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-06-19 13:04:20 -0400

    New branch

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   # GET /items
   # GET /items.json
   def index
-    @items = Item.all
+    @items = Item.find(params[:list_id])
   end
 
   # GET /items/1
