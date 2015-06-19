93d2acaf5c98a9aadf9d0ba3a1a4473b1cc1403f
Daisy Magnus-Aryitey <daisymagnusaryitey@gmail.com>
2015-06-19 12:51:17 -0400

    generate scaffold for items and list

---
+++ app/views/items/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @item, :id, :description, :due_date, :order, :list_id, :created_at, :updated_at
---
+++ db/migrate/20150619164955_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.text :description
+      t.date :due_date
+      t.integer :order
+      t.integer :list_id
+
+      t.timestamps null: false
+    end
+  end
+end
