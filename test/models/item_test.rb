6a904487785138eeccb0781593738c2b9e800964
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-06-22 08:53:51 -0400

    Add back in assign order test

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "assign order" do
-  #   assert_equal 5, Item.assign_order
-  # end
+  test "assign order" do
+    assert_equal 5, Item.assign_order
+  end
 end
